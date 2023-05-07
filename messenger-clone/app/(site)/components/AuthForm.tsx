20aa3ba5937f23079a339b632bab45963562bc58
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-07 20:01:53 +0200

    7.1-logout added

--- messenger-clone/app/(site)/components/AuthForm.tsx
+++ messenger-clone/app/(site)/components/AuthForm.tsx
@@ -1,20 +1,29 @@
 'use client'
 
 import Input from "../../components/inputs/Input"
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { FieldValues,useForm,SubmitHandler } from "react-hook-form"
 import Button from "../../components/Button"
 import AuthSocialButton from "./AuthSocialButton"
 import {BsGithub, BsGoogle} from 'react-icons/bs'
 import axios from "axios"
 import {toast} from 'react-hot-toast'
-import {signIn} from 'next-auth/react'
+import {signIn, useSession} from 'next-auth/react'
+import {useRouter} from 'next/navigation'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
 const AuthForm = () => {
+  const session = useSession()
+  const router = useRouter()
   const [variant,setVariant] = useState<Variant>('LOGIN')
   const [isLoading,setIsLoading] = useState(false)
+
+  useEffect(() => {
+    if(session?.status == 'authenticated') {
+      router.push('/users')
+    }
+  },[session?.status,router])
 
   const toggleVariant = useCallback(() => {
     if (variant === 'LOGIN') {
@@ -44,6 +53,7 @@ const AuthForm = () => {
 
     if (variant === 'REGISTER') {
       axios.post('/api/register/',data)
+      .then(() => signIn('credentials',data))
       .catch(() => toast.error('Something went wrong'))
       .finally(() => setIsLoading(false))
     }
@@ -58,6 +68,7 @@ const AuthForm = () => {
         }
         if (callback?.ok && !callback.error) {
           toast.success("Logged in")
+          router.push('/users')
         }
       })
       .finally(() => setIsLoading(false))
