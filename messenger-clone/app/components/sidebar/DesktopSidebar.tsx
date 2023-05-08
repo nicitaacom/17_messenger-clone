3242f8ebe71d6c6a47e0d4f50cb7f81a24ff73f5
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-08 14:55:11 +0200

    7.3 desktop sidebar current user done

--- messenger-clone/app/components/sidebar/DesktopSidebar.tsx
+++ messenger-clone/app/components/sidebar/DesktopSidebar.tsx
@@ -3,10 +3,17 @@
 import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser:User
+}
+
+const DesktopSidebar:React.FC<DesktopSidebarProps> = ({currentUser}) => {
   const routes = useRoutes()
   const [isOpen,setIsOpen] = useState(false)
+
+  console.log(currentUser)
 
   return ( 
     <div 
