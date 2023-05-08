72dc044f56d3a432636520b4815da5b933a9670f
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-08 12:38:31 +0200

    7.3-sidevar-nav added
    with sidebar 2:26:58

---
+++ messenger-clone/app/components/sidebar/DesktopSidebar.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import useRoutes from "@/app/hooks/useRoutes";
+import { useState } from "react";
+
+const DesktopSidebar = () => {
+  const routes = useRoutes
+  const [isOpen,setIsOpen] = useState(false)
+
+  return ( 
+    <div 
+    className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-auto
+    lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
+      <nav className="mt-4 flex flex-col justify-between">
+        <ul className="flex flex-col items-center space-y-1" role="list">
+         
+        </ul>
+      </nav>
+      </div>
+   );
+}
+ 
+export default DesktopSidebar;
