345035b720e07d98be9ed9561a57a123af70a551
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-08 14:36:36 +0200

    7.3 mobile sidebar done

--- messenger-clone/app/components/sidebar/MobileFooter.tsx
+++ messenger-clone/app/components/sidebar/MobileFooter.tsx
@@ -1,8 +1,24 @@
 'use client'
 
+import useConversation from "@/app/hooks/useConversation";
+import useRoutes from "@/app/hooks/useRoutes";
+import MobileItem from "./MobileItem";
+
 const MobileFooter = () => {
+  const routes = useRoutes()
+  const {isOpen} = useConversation()
+
+  if (isOpen) {
+    return null
+  }
   return ( 
-    <div>Mobile footer</div>
+    <div className="fixed justify-between w-full bottom-0 z-40 flex items-center bg-white
+    border-t-[1px] lg:hidden">
+      {routes.map((route) => (
+        <MobileItem key={route.href}
+        href={route.href} active={route.active} icon={route.icon} onClick={route.onClick}/>
+      ))}
+    </div>
    );
 }
  
