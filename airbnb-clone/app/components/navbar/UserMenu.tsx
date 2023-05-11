09e67ee8ad5f53ac4a70a243b70586685af67525
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-11 16:22:38 +0200

    airbnb_4-login-logic done
    2:09:33 - 2:28:12

--- airbnb-clone/app/components/navbar/UserMenu.tsx
+++ airbnb-clone/app/components/navbar/UserMenu.tsx
@@ -5,9 +5,16 @@ import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { signOut } from 'next-auth/react';
 
-const UserMenu = () => {
+interface UserMenuProps {
+  currentUser?: User | null
+}
+
+const UserMenu:React.FC<UserMenuProps> = ({currentUser}) => {
   const registerModal = useRegisterModal()
+  const loginModal = useLoginModal()
   const [isOpen,setIsOpen] = useState(false)
 
   const toggleOpen = useCallback(() => {
@@ -35,10 +42,23 @@ const UserMenu = () => {
         <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden
         right-0 '>
           <div className='flex flex-col cursor-pointer'>
+            {currentUser ? (
+              <>
+              <MenuItem label='Myrips' onClick={() => {}}/>
+              <MenuItem label='My favorites' onClick={() => {}}/>
+              <MenuItem label='My reservations' onClick={() => {}}/>
+              <MenuItem label='My properties' onClick={() => {}}/>
+              <MenuItem label='Airbnb my home' onClick={() => {}}/>
+              <hr/>
+              <MenuItem label='Logout' onClick={() => signOut()}/>
+              </>
+            )
+            : (
             <>
-              <MenuItem label='Login' onClick={() => {}}/>
+              <MenuItem label='Login' onClick={loginModal.onOpen}/>
               <MenuItem label='Sign up' onClick={registerModal.onOpen}/>
             </>
+            )}
           </div>
         </div>
       )}
