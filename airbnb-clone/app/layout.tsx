e5ca74a23a54173da9f91804b5415110f19165df
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-11 14:12:26 +0200

    airbnb_2-auth-ui done
    34:32 - 1:35:27

--- airbnb-clone/app/layout.tsx
+++ airbnb-clone/app/layout.tsx
@@ -1,6 +1,9 @@
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Nunito } from 'next/font/google'
+import ToasterProvider from './providers/ToasterProvider'
 
 
 export const metadata = {
@@ -20,7 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <ToasterProvider/>
+          <RegisterModal/>
+          <Navbar/>
+        </ClientOnly>
         {children}
         </body>
     </html>
