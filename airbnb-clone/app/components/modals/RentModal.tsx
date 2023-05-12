e05e6dd1100b998576539009f29c54da29dd77ef
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-12 19:09:41 +0200

    airbnb_9-step-3 done
    4:07:46 - 4:17:16

--- airbnb-clone/app/components/modals/RentModal.tsx
+++ airbnb-clone/app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
 
 enum STEPS {
@@ -41,6 +42,9 @@ const RentModal = () => {
 
   const category = watch('category')
   const location = watch('location')
+  const guestCount = watch('guestCount')
+  const roomCount = watch('roomCount')
+  const bathroomCount = watch('bathroomCount')
 
   const Map = useMemo(() => dynamic(() => import('../Map'), {
     ssr:false
@@ -101,6 +105,22 @@ const RentModal = () => {
         <Heading title='Where is your place located?' subTitle='Help guests find you'/>
         <CountrySelect onChange={(value) => setCustomValue('location',value)} value={location}/>
         <Map center={location?.latlng}/>
+      </div>
+    )
+  }
+
+  if (step == STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading title='Share some basics about your place' subTitle='What amenities do you have?'/>
+        <Counter title='Guests' subTitle='How many guests do you allow?' value={guestCount}
+        onChange={(value) => setCustomValue('guestCount',value)}/>
+        <hr />
+        <Counter title='Rooms' subTitle='How many rooms do you have?' value={roomCount}
+        onChange={(value) => setCustomValue('roomCount',value)}/>
+        <hr />
+        <Counter title='Bathrooms' subTitle='How many bathrooms do you have?' value={bathroomCount}
+        onChange={(value) => setCustomValue('bathroomCount',value)}/>
       </div>
     )
   }
