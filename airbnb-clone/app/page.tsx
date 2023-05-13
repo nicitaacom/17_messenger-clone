66c6f39ff0cb1587157d4eaf6476889756fa0d10
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-13 14:11:58 +0200

    airbnb_12-fetching-listings done
    4:59:10 - 5:23:02

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -1,10 +1,13 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings()
+  const currentUser = await getCurrentUser()
 
   if (listings.length == 0) {
     return (
@@ -21,8 +24,8 @@ export default async function Home() {
           <div>
             {listings.map((listing:any) => {
               return (
-                <div>{listing.title}</div>
-              )
+                  <ListingCard currentUser={currentUser} key={listing.id} data={listing}/>
+                )
             })}
           </div>
         </div>
