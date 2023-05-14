5c70d4d9069e3e257afbb8c8a196c160ba080c7b
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-14 14:42:21 +0200

    airbnb_15+16-listing-reservation+logic done

--- airbnb-clone/app/listings/[listingId]/page.tsx
+++ airbnb-clone/app/listings/[listingId]/page.tsx
@@ -4,6 +4,7 @@ import getListingById from "@/app/actions/getListingById";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import LisitngClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
   listingId?:string
@@ -11,7 +12,9 @@ interface IParams {
 
 const ListingPage = async ({params}:{params:IParams}) => {
   const listing = await getListingById(params)
+  const reservations = await getReservations(params)
   const currentUser = await getCurrentUser()
+
 
   if (!listing) {
     return (
@@ -22,7 +25,7 @@ const ListingPage = async ({params}:{params:IParams}) => {
   }
   return ( 
     <ClientOnly>
-      <LisitngClient listing={listing} currentUser={currentUser}/>
+      <LisitngClient reservations={reservations} listing={listing} currentUser={currentUser}/>
     </ClientOnly>
    );
 }
--- airbnb-clone/app/types/index.ts
+++ airbnb-clone/app/types/index.ts
@@ -1,8 +1,11 @@
-import {Listing, User} from '@prisma/client'
+import {Listing, Reservation, User} from '@prisma/client'
 
 export type SafeListing = Omit<Listing,'createdAt'>& {
   createdAt:string
 }
+
+export type SafeReservation = Omit<Reservation,'createdAt' | 'startDate' | 'endDate' | 'listing'>&
+{createdAt:string,startDate:string,endDate:string,listing:SafeListing}
 
 export type SafeUser = Omit<User,'createdAt' | 'updatedAt' | 'emailVerified'>
 &{createdAt:string,updatedAt:string,emailVerified:string | null}
