df03b9d7f4ddd83a2109e1b222155743ffaf7d31
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-28 15:56:37 +0200

    ! deleted

--- app/hooks/useConversation.ts
+++ app/hooks/useConversation.ts
@@ -5,7 +5,7 @@ export default function useConversation() {
   const params = useParams();
 
   const conversationId = useMemo(() => {
-    if (!params.conversationId) {
+    if (params.conversationId) {
       return "";
     }
     return params.conversationId as string;
