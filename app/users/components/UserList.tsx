560d4df20c2fac6705c9533516404d34e25a3956
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-28 15:47:52 +0200

    contrast impovement

--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -12,7 +12,7 @@ export default function UserList({ items }: UserListProps) {
     <aside className="fixed inset-y-0 pb-20 lg:pb-0 lg:left-20 lg:w-80 lg:block overflow-y-auto border-r border-gray-200 blocl w-full left-0">
       <div className="px-5">
         <div className="flex-col">
-          <div className="text-2xl font-bold text-neutral-800 py-4">People</div>
+          <div className="text-2xl font-bold text-neutral-300 py-4">People</div>
         </div>
         {items.map((item) => (
           <UserBox key={item.id} data={item} />
